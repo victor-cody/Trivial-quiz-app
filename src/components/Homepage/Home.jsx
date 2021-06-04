a45114098fbe8a654bfa1b63cf7e2cdbd814b0bd
Victor-Cody <vickyikechukwu13@gmail.com>
2021-06-04 15:05:13 +0100

    edited links and homepage  styles

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
 	return (
 		<div className='container w-100 h-100 d-flex flex-column  justify-content-center align-items-center'>
 			<div className="d-flex flex-column align-items-center justify-content-center">
-				<h1 className='text-title h1 text-center'>Ready To Challenge Your Mind?</h1>
+				<h1 className='text-capitalize display-4 text-center'>Ready To Challenge Your Mind?</h1>
 
-				<div className='mt-5' >
-					<Link className="a text-title btn btn-default">let's play</Link>
-					<Link className="a text-title btn btn-default">check highscores</Link>
+				<div className='mt-5 row justify-content-spacearound align-items-spacearound' >
+					<Link className="col-sm-9  a text-capitalize font-weight-bold btn mx-1 my-2">let's play</Link>
+					<Link className="col-sm-9  a text-capitalize font-weight-bold btn mx-1 my-2">check highscores</Link>
 				</div>
 
 			</div>
---
+++ src/components/Pre-Game/PreGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreGame = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default PreGame;
