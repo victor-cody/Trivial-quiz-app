fb29903b9a1a1b91a1bc8c508bf291d5f9209e70
Victor-Cody <vickyikechukwu13@gmail.com>
2021-06-04 13:13:09 +0100

    initial commit :D

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//jQuery
+// import jQuery from './assets/jquery/JQUERY'
+//bootstrap
+import './assets/bootstrap/bootstrap.min.css';
+import './assets/bootstrap-icons/bootstrap-icons.css';
+//animations
+// import './assets/wow/wow.min.js'
+import './assets/animate/_animations.scss'
+
+import './assets/sass/App.scss';
+
+
+function App() {
+
+  return (
+    <>
+    <Router>
+
+    </Router>
+        
+    </>
+  );
+}
+
+export default App;
