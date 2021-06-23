a5081efb4aa4333f3e5279b8eeb9ef7054eb441a
Victor-Cody <vickyikechukwu13@gmail.com>
2021-06-23 10:35:55 +0100

    wrote onClick callback function for the GameOption component

--- src/components/game-option/GameOption.js
+++ src/components/game-option/GameOption.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import './_option-styles.scss';
 
-const GameOption = ({ number, text, id, answer }) => {
+const GameOption = ({ number, text, id, callback }) => {
 
-	//when a question choice is clicked
-	function checkChoice (e, param) {
-		const selectedChoice = e.target;
-		const correctAnswer = document.querySelector(`p[data-choicenumber= "${answer}}"]`) ?? null;
-		//Check if selected answer is wrong or right and apply corresponding class style
-		if (selectedAnswer == currentQuestion.answer) {
-			selectedChoice.classList.add('correct');
-			incrementScore(BONUS);
-
-		} else {
-			selectedChoice.classList.add('incorrect');
-			setTimeout(() => {
-				correctAnswer.classList.add('correct');
-			}, 1500);
-
-		}
-	}
 	return (
 			
-			<div class="choice row align-items-center border-white mb-3 rounded-sm bg-light">
+			<div class="choice row align-items-center border-white mb-3 rounded-sm bg-light"
+			onClick = {callback}
+			>
 				<p class="choice__option col-md-3 px-3 py-3">{number}</p>
 				<p class="choice__text col-md-9 px-3 py-3" data-choiceNumber={id}>{text}</p>
 			</div>
--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useHarperDB } from "use-harperdb";
+//Components
 import ProgressBar from '../../components/ProgressBar/ProgressBar'
 import GameOption from '../../components/game-option/GameOption';
 import { GameContex } from '../../contex/GameContex';
@@ -9,116 +10,162 @@ import Countdown from '../../components/Countdown/Countdown';
 
 const Game = () => {
 
-const history = useHistory();
+	const history = useHistory();
 
-//States
-const [availableQuestions, setAvailableQuestions] = useState([]);
-const [questionCounter, setQuestionCounter] = useState(0)
-const [score, setScore] = useState(0)
-const [showNextButton, setShowNextButton] = useState(false)
-const [currentQuestion, setcurrentQuestion] = useState({})
-const [countDown, setCountDown] = useState(true)
-const [timer, setTimer] = useState(0)
+	//States
+	const [availableQuestions, setAvailableQuestions] = useState([]);
+	const [questionCounter, setQuestionCounter] = useState(0)
+	const [score, setScore] = useState(0)
+	const [showNextButton, setShowNextButton] = useState(false)
+	const [currentQuestion, setcurrentQuestion] = useState({})
+	const [countDown, setCountDown] = useState(true)
+	const [timer, setTimer] = useState(0)
+	const [timerID, setTimerID] = useState(null)
 
-const { chosenCategory, BONUS, MAX_QUESTIONS } = useContext(GameContex)
+	const { chosenCategory, BONUS, MAX_QUESTIONS, QUESTION_TIME } = useContext(GameContex)
 
-// Querying the database
-const [questions, loading, error, refresh] = useHarperDB({
-	query: { operation: 'sql', sql: `select * from quizzes.${chosenCategory}` },
-});
+	// Querying the database
+	const [questions, loading, error, refresh] = useHarperDB({
+		query: { operation: 'sql', sql: `select * from quizzes.${chosenCategory}` },
+	});
 
-useEffect(() => {
-	setAvailableQuestions(...questions)
-	setScore(0)
-	setQuestionCounter(0)
-	return () => {
+	useEffect(() => {
+		setAvailableQuestions(...questions)
+		setScore(0)
+		setQuestionCounter(0)
+		setTimerID(setInterval(countDownTime, 1000))
+		return () => {
 
+		}
+		//@eslint-ignore-next-line
+	}, [chosenCategory, questions])
+
+
+	const getNewQuestion = () => {
+		//Go to end game page if all questions have been rendered
+		if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+			localStorage.setItem('mostRecentScore', score);
+			localStorage.setItem('maxScore', MAX_QUESTIONS * BONUS);
+			return history.push("/end");
+		}
+
+		//else, first hide the next button
+		setShowNextButton(false)
+
+		//then select and render random question from questions array
+		setQuestionCounter(prev => prev++);
+		const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+		setcurrentQuestion(availableQuestions[questionIndex]);
+
+		//Remove just rendered question from the question array
+		setAvailableQuestions(prev => prev.splice(questionIndex, 1))
+
+		//Start the count down
+		setCountDown(true);
 	}
-}, [chosenCategory, questions])
 
-
-const getNewQuestion = () => {
-	//Go to end game page if all questions have been rendered
-	if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-		localStorage.setItem('mostRecentScore', score);
-		localStorage.setItem('maxScore', MAX_QUESTIONS * BONUS);
-		return history.push("/end");
+	//Timer function
+	const countDownTime = () => {
+		if (countDown === true && timer <= QUESTION_TIME) {
+			setTimer(prev => prev++)
+		}
+		else {
+			setTimer(0)
+			getNewQuestion();
+		}
 	}
 
-	//else, first hide the next button
-	setShowNextButton(false)
+	//when a question choice is clicked
+	const evaluateChoice = (e) => {
+		if (!countDown) {
+			return;
+		}
 
-	//then select and render random question from question array
-	setQuestionCounter(prev => prev++);
+		setCountDown(false)
+		clearInterval(timerID)
 
-	const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-	setcurrentQuestion(availableQuestions[questionIndex]);
+		const selectedChoice = e.target;
+		const correctAnswer = document.querySelector(`p[data-choicenumber = "${currentQuestion.answer}"]`);
 
-	//Remove just rendered question from the question array
-	setAvailableQuestions(prev => prev.splice(questionIndex, 1))
+		//Check if selected answer is wrong or right and apply corresponding class style
+		if (selectedChoice.dataset["choicenumber"] === currentQuestion.anwser) {
+			selectedChoice.classList.add('correct');
+			setScore(BONUS);
+		} else {
+			selectedChoice.classList.add('incorrect');
+			setTimeout(() => {
+				correctAnswer.classList.add('correct')
+			}, 1200);
+		}
 
-	//Start the count down
-	setCountDown(true);
-}
+		//show next question button
+		setTimeout(() => {
+			setShowNextButton(true)
+		}, 2000);
+	}
 
-if (loading) {
-	return (
-		<h2>Loading</h2>
-	)
-}
-// start game if their are available questions or if we haven't exceeded the maximum number of questions
-if ((availableQuestions && availableQuestions.length > 0) || questionCounter <= MAX_QUESTIONS) {
-	return (
-		<section>
-			<h2>{chosenCategory}</h2>
+	if (loading) {
+		return (
+			<h2>Loading...</h2>
+		)
+	}
+	// start game if their are available questions or if we haven't exceeded the maximum number of questions
+	if ((availableQuestions && availableQuestions.length > 0) || questionCounter <= MAX_QUESTIONS) {
+		return (
+			<section>
+				<h2>{chosenCategory}</h2>
 
-			<header className="header">
+				<header className="header">
 
-				<div className='d-flex align-items-center justify-content-between flex-column' >
+					<div className='d-flex align-items-center justify-content-between flex-column' >
 
-					<p className="counter text-center h2">Question {questionCounter}/{MAX_QUESTIONS} </p>
-					<ProgressBar counter={questionCounter} />
-				</div>
+						<p className="counter text-center h2">
+							Question {questionCounter}/{MAX_QUESTIONS} </p>
+						<ProgressBar counter={questionCounter} />
+					</div>
 
-				<div className='d-flex align-items-center justify-content-between  flex-column' >
-					<p className="text-center h2">Score</p>
-					<h2 className="score display-3 font-weight-bold">{score}</h2>
-				</div>
+					<div className='d-flex align-items-center justify-content-between  flex-column' >
+						<p className="text-center h2">Score</p>
+						<h2 className="score display-3 font-weight-bold">{score}</h2>
+					</div>
 
-			</header>
+				</header>
 
-			{	currentQuestion.answers.map((answer, id) => (
-				<GameOption
-					number={id + 1}
-					text={answer}
-					id={id}
-				/>
-			))}
+				{	currentQuestion.answers.map((answer, id) => (
+					<GameOption
+						key={'choice' + id}
+						number={id + 1}
+						text={answer}
+						id={id}
+						callback={evaluateChoice}
+					/>
+				))}
 
-			<Countdown counter={timer} />
+				<Countdown counter={timer} />
 
-			{showNextButton && <button className="col-sm-7 a text-capitalize font-weight-bold btn mt-3 mb-1"
-				onClick={(e) => {
-					e.preventDefault();
-					// removing the 'correct' and 'incorrect' class from each of the options element
-					[...document.getElementsByClassName('choice-container')].forEach(element => {
-						element.classList.remove('correct')
-						element.classList.remove('incorrect')
-					});
-					// getting a new question
-					getNewQuestion()
-				}}
-			>continue</button>}
-		</section>
-	);
-}
-else {
-	return (
-		<div>
-			{error}
-		</div>
-	)
-}
+				{showNextButton && <button className="col-sm-7 a text-capitalize font-weight-bold btn mt-3 mb-1"
+					onClick={(e) => {
+						e.preventDefault();
+						// removing the 'correct' and 'incorrect' class from each of the options element
+						[...document.getElementsByClassName('choice-container')].forEach(element => {
+							element.classList.remove('correct')
+							element.classList.remove('incorrect')
+						});
+						// getting a new question
+						getNewQuestion()
+					}}
+				>continue</button>}
+			</section>
+		);
+	}
+
+	else {
+		return (
+			<div>
+				{error}
+			</div>
+		)
+	}
 
 
 };
