d94e9319be82e33d52f3692834f29c081c0a9637
Victor-Cody <vickyikechukwu13@gmail.com>
2021-07-01 03:35:06 +0100

    create: user's profile image component

---
+++ src/components/buy-me-a-coffe/BmcButton.js
@@ -0,0 +1,14 @@
+
+const BmcButton = () => {
+	return (
+		<a href="https://www.buymeacoffee.com/molipa" target="_blank" rel="noreferrer" className="btn shine">
+			<img
+				src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+				alt="Buy Me A Coffee"
+				className="h-"
+			/>
+		</a>
+	);
+};
+
+export default BmcButton;
---
+++ src/components/user-profile-image/ProfileImage.js
@@ -0,0 +1,22 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import './styles.scss';
+
+const ProfileImage = () => {
+	const { user } = useAuth0();
+	const { name, picture} = user ;
+	return (
+		<Link className="d-flex align-items-center justify-content-between rounded-md px-2 py-2 bg-light profile">
+			<span className="text-dark font-weight-bold profile-name">
+				{name}
+			</span>
+			<img
+				src={picture}
+				alt={`${user}`}
+				className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+			/>
+		</Link>
+	);
+};
+
+export default ProfileImage;
